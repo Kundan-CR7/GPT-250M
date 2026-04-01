3346e4be81c60b4d39526d7c0cb6f04942d6af61
Kundan-CR7 <Kundangupta7334@gmail.com>
2026-04-02 01:27:17 +0530

    training loop implementation

--- train.py
+++ train.py
@@ -1,7 +1,9 @@
 import torch
 from torch.utils.data import dataloader
+import torch.nn.functional as F
 from config import GPTConfig
 from dataset import GPTDataset
+from model import GPT
 
 config = GPTConfig()
 
@@ -17,4 +19,34 @@ train_loader = dataloader(
     num_workers = 2
 )
 
-x,y = next(iter(train_loader))
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Training on device: {device}")
+
+model = GPT(config)
+model.to(device)
+
+learning_rate = 3e-4
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+epochs = 100
+
+model.train()
+
+for step in range(epochs):
+    x,y = next(iter(train_loader))
+    x = x.to(device)
+    y = y.to(device)
+
+    logits = model(x)
+
+    B,T,C = logits.shape
+    logits_flat = logits.view(B*T,C)
+    y_flat = y.view(B*T)
+
+    loss = F.cross_entropy(logits_flat,y_flat)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+    print(f"Step {step} | Loss: {loss.item():.4f}")
+
